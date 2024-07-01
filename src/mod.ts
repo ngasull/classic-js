d559e8b533e79a024556cc20cdcf46613da601af
Nicolas Gasull <nicolas@gasull.net>
2024-07-01 16:46:05 +0200

    Expose indexedUris

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,12 @@
-export { client, inline, js, jsResources, mkRef, toJS, unsafe } from "./js.ts";
+export {
+  client,
+  indexedUris,
+  inline,
+  js,
+  jsResources,
+  mkRef,
+  toJS,
+  unsafe,
+} from "./js.ts";
 export { isJSable } from "./types.ts";
 export type { Activation, JS, JSable, JSOverrides } from "./types.ts";
